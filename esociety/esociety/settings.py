a0126e38bf9c8050da86e731a6e7b516c2686443
Muhammad Abu Bakar <refocuse313@gmail.com>
2020-11-02 22:49:21 +0500

    working to integrate static files

--- esociety/esociety/settings.py
+++ esociety/esociety/settings.py
@@ -137,9 +137,3 @@ STATICFILES_FINDERS = (
 
 
 STATIC_DIR = os.path.join(BASE_DIR,'static')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    STATIC_DIR,
-]
