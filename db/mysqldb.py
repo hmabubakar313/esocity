1f777be89f178c1f93228e0df459ad1785800567
Muhammad Abu Bakar <refocuse313@gmail.com>
2020-11-26 00:39:04 +0500

    inserting data into DB

--- db/mysqldb.py
+++ db/mysqldb.py
@@ -1,49 +1,54 @@
-import mysql.connector
+import mysql.connector as mysql
 
 #establishing the connection
-conn = mysql.connector.connect(
+conn = mysql.connect(
    user='root', password='admin', host='localhost', database='esociety'
 )
 
+print(conn)
 #Creating a cursor object using the cursor() method
 cursor = conn.cursor()
 
-#Dropping EMPLOYEE table if already exists.
+# *Dropping EMPLOYEE table if already exists.
 cursor.execute("DROP TABLE IF EXISTS STUDENT")
 
-#Creating table as per requirement
-sql ='''CREATE TABLE STUDENT(
-   USERNAME DOUBLE NOT NULL,
-   E_MAIL DOUBLE,
-   PASSWORD CHAR(250),
-   REG_NO DOUBLE
-)'''
-cursor.execute(sql)
 
-#Closing the connection
+# Creating table as per requirement
+sql ='''CREATE TABLE student(
+   username VARCHAR(255) NOT NULL,
+   e_mail VARCHAR(255))'''
+# cursor.execute(sql)
+cursor.execute("SHOW TABLES")
+tables = cursor.fetchall()
+ ## it returns list of tables present in the database
+for table in tables:
+    print(table)
+
+
+# #Closing the connection
 conn.close()
-# * insering into database
-conn = mysql.connector.connect(
-   user='root', password='admin', host='localhost', database='esociety')
+# # * insering into database
+# conn = mysql.connector.connect(
+   # user='root', password='admin', host='localhost', database='esociety')
 
-#Creating a cursor object using the cursor() method
-cursor = conn.cursor()
+# #Creating a cursor object using the cursor() method
+# cursor = conn.cursor()
 
-# Preparing SQL query to INSERT a record into the database.
-sql = """INSERT INTO STUDENT(
-   USERNAME,E_MAIL , PASSWORD, REG_NO,)
-   VALUES ('admin', 'admin@admin.com', 'admin', 'abc-123',)"""
+# # Preparing SQL query to INSERT a record into the database.
+# sql = """INSERT INTO STUDENT(
+#    USERNAME,E_MAIL , PASSWORD, REG_NO,)
+#    VALUES ('admin','admin@.com', 123,123 )"""
 
-try:
-   # Executing the SQL command
-   cursor.execute(sql)
-    #  Commit your changes in the database
-   conn.commit()
+# try:
+#    # Executing the SQL command
+#    cursor.execute(sql)
+#     #  Commit your changes in the database
+#    conn.commit()
 
-except: 
-     conn.rollback()
-   # Rolling back in case of error
+# except: 
+#      conn.rollback()
+#    # Rolling back in case of error
   
 
-# Closing the connection
-conn.close()
+# # Closing the connection
+# conn.close()
