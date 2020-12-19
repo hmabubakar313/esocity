a2713bb5b23b00619e7bedd24efd39c14f398491
Muhammad Abu Bakar <refocuse313@gmail.com>
2020-12-20 00:55:47 +0500

    extracted request data using "GET" method

--- esociety/esociety/views.py
+++ esociety/esociety/views.py
@@ -10,23 +10,21 @@ def login(request):
 
 
 def signup(request):
+    print(request.GET)
     return render(request,"signup.html")
 
 def save_signup(request):
-    if request.method=='POST':
-    message_data = request.POST['message']        
-    return render(request,"test.html")
+    # print(request.GET)     
+    # return render(request,"test.html")
+    
     """  conn = mysql.connect(
    user='root', password='admin', host='localhost', database='esociety'
 )
-
     print(conn)
     #Creating a cursor object using the cursor() method
     cursor = conn.cursor()
-
     # *Dropping EMPLOYEE table if already exists.
     cursor.execute("DROP TABLE IF EXISTS students")
-
     # Creating table as per requirement
     # #Closing the connection
     conn.close()
